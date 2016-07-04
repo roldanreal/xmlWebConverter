62511d8180b8217faa634814217f66813d38e328
Roldan <roldanreal@yahoo.com.ph>
2016-07-04 13:21:17 +0800

    XMLCOnverter

--- src/main/java/ph/com/globe/csvparser/xmlconverter/XMLConverter.java
+++ src/main/java/ph/com/globe/csvparser/xmlconverter/XMLConverter.java
@@ -88,65 +88,68 @@ public class XMLConverter {
                 
               //here
             	Iterator<Map.Entry<String,String>> iterator = xmlMassRequestHeadersMap.entrySet().iterator();
-                while(iterator.hasNext()) {
-                    Map.Entry<String,String> entry = iterator.next();
-                    //writer.append(" " + entry.getKey() + "=\"" + entry.getValue() +"\"");
-                    if(entry.getKey().equals("requestCreationDate") && entry.getValue() !=null){
-                    	requestCreationDt =  entry.getValue();
-                    }
-                    if(entry.getKey().equals("requestExecutionDate") && entry.getValue()!=null){
-                    	requestExecutionDt = entry.getValue();
-                    }
-                    if(entry.getKey().equals("dateFormat") && entry.getValue()!=null){
-                    	dateFormat = entry.getValue();
-                    }
-                    if(entry.getKey().equals("LOCK_IN_START_DATE") && entry.getValue()!=null){
-                    	lockInStart = entry.getValue();
-                    }
-                    if(entry.getKey().equals("LOCK_IN_END_DATE") && entry.getValue()!=null){
-                    	lockInEnd = entry.getValue();
-                    }
-                   
-                }
-                if(isValidDateFormat(dateFormat, requestCreationDt) == false){
-                	errorMsg = errorMsg.concat("Invalid request creation date format. <br/>");
-                }else{
-                	isCreationDtValid = true;
-                }
+            	requestCreationDt = xmlMassRequestHeadersMap.get("requestCreationDate");
+            	requestExecutionDt = xmlMassRequestHeadersMap.get("requestExecutionDate");
+            	dateFormat = xmlMassRequestHeadersMap.get("dateFormat");
+            	lockInStart = xmlMassRequestHeadersMap.get("LOCK_IN_START_DATE");
+            	lockInEnd = xmlMassRequestHeadersMap.get("LOCK_IN_END_DATE");
+            	System.out.println("Request creation date: " + requestCreationDt);
+            	System.out.println("Request requestExecutionDate : " + requestExecutionDt);
+            	System.out.println("Request dateFormat: " + dateFormat);
+            	System.out.println("Request lockInStart: " + lockInStart);
+            	System.out.println("Request lockInEnd: " + lockInEnd);
+            	
+            	if(dateFormat!=null) {
+            		if (requestCreationDt!=null) {
+            			if(!isValidDateFormat(dateFormat, requestCreationDt)){
+                        	errorMsg = errorMsg.concat("Invalid request creation date format. <br/>");
+                        }else{
+                        	isCreationDtValid = true;
+                        }
+            		}
+            		if(requestExecutionDt!=null) {
+                        if(!isValidDateFormat(dateFormat,requestExecutionDt)){
+                        	errorMsg = errorMsg.concat("Invalid request execution date format. <br/>");
+                        }else{
+                        	isExecutionDtValid = true;
+                        }
+            		}
+            		if(lockInStart!=null) {
+                      if(!isValidDateFormat(dateFormat,lockInStart)){
+                    	errorMsg = errorMsg.concat("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Invalid lock in start date format. <br/>");
+                      }else{
+                    	  isLockInStartDateValid = true;
+                      }
+            		}
+            	}
+
+                
              
-                if(isValidDateFormat(dateFormat,requestExecutionDt)== false){
-                	errorMsg = errorMsg.concat("Invalid request execution date format. <br/>");
-                }else{
-                	isExecutionDtValid = true;
-                }
-                /*if(isValidDateFormat(dateFormat,lockInStart)== false){
-                	errorMsg = errorMsg.concat("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Invalid lock in start date format. <br/>");
-                }else{
-                	isLockInStartDateValid = true;
-                }
-                if(isValidDateFormat(dateFormat,lockInEnd)== false){
-                	errorMsg = errorMsg.concat("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Invalid lock in end date format. <br/>");
-                }else{
-                	isLockInEndDateValid = true;
-                }*/
-                
-                if(isCreationDtValid && isExecutionDtValid){
-                	creationDate = convertStringToDate(dateFormat, requestCreationDt);
-                	executionDate = convertStringToDate(dateFormat, requestExecutionDt);
-                	
-                	 if(creationDate.compareTo(executionDate)>0){
-                         errorMsg = errorMsg.concat("Request Creation Date is greater than Request Execution Date.");
-                     }
-                }
-                
-                /*if(isLockInStartDateValid && isLockInEndDateValid){
-                    lockInStartDate = convertStringToDate(dateFormat, lockInStart);
-                	lockInEndDate = convertStringToDate(dateFormat, lockInEnd);
-                	
-                	 if(lockInStartDate.compareTo(lockInEndDate)>0){
-                         errorMsg = errorMsg.concat("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Lock In Start Date is greater than Lock In End Date");
-                     }
-                }*/
+
+
+//                if(isValidDateFormat(dateFormat,lockInEnd)== false){
+//                	errorMsg = errorMsg.concat("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Invalid lock in end date format. <br/>");
+//                }else{
+//                	isLockInEndDateValid = true;
+//                }
+//                
+//                if(isCreationDtValid && isExecutionDtValid){
+//                	creationDate = convertStringToDate(dateFormat, requestCreationDt);
+//                	executionDate = convertStringToDate(dateFormat, requestExecutionDt);
+//                	
+//                	 if(creationDate.compareTo(executionDate)>0){
+//                         errorMsg = errorMsg.concat("Request Creation Date is greater than Request Execution Date.");
+//                     }
+//                }
+//                
+//                if(isLockInStartDateValid && isLockInEndDateValid){
+//                    lockInStartDate = convertStringToDate(dateFormat, lockInStart);
+//                	lockInEndDate = convertStringToDate(dateFormat, lockInEnd);
+//                	
+//                	 if(lockInStartDate.compareTo(lockInEndDate)>0){
+//                         errorMsg = errorMsg.concat("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Lock In Start Date is greater than Lock In End Date");
+//                     }
+//                }
             	//ends here
                 System.out.println("file destination: " + destination);
                 File xmlFile = new File(destination);
